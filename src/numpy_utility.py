ebc6e82d4102891b845158d12dcd7b7d67e3245b
styvesg <stayves@musc.edu>
2017-06-09 22:18:19 -0400

    update utility

--- src/numpy_utility.py
+++ src/numpy_utility.py
@@ -54,3 +54,65 @@ def make_gaussian_mass_stack(xs, ys, sigmas, n_pix, size=None, dtype=np.float32)
     for i in range(1,stack_size):
         _,_,Z[i,:,:] = make_gaussian_mass(xs[i], ys[i], sigmas[i], n_pix, size=size, dtype=dtype)
     return X, Y, Z
+
+
+
+def pruning_mask(shaped_as, prune_ratio=0.0):
+    '''prune_ratio = 1. means discard everything'''
+    return np.random.choice([True, False], size=shaped_as.shape[1], replace=True, p=[1.-prune_ratio, prune_ratio])#astype(np.bool)
+    
+def uniform_nsphere(batch, size):
+    '''Returns a batch of uniformly distributed points on a nsphere'''
+    nns = np.random.normal(0., 1., size=(batch, size)).astype(np.float32)
+    nnss = np.sqrt(np.sum(np.square(nns), axis=1))
+    return (nns.T / nnss).T
+
+def uniform_ncube(batch, size):
+    return np.random.uniform(-1., 1., size=(batch, size)).astype(np.float32) 
+
+def normal_ncube(batch, size):
+    return np.random.normal(0., 1., size=(batch, size)).astype(np.float32)
+
+
+
+def sie(x, c=10): 
+    '''SparseIntegerEmbedding'''
+    y = np.zeros((len(x), c), dtype=np.float32)
+    y[np.arange(len(x)), x] = 1
+    return y
+
+def place_tile_in(tile, new_npx):
+    batch_size = tile.shape[0]
+    features = tile.shape[1]
+    A = np.zeros(shape=(batch_size, features, new_npx, new_npx), dtype=tile.dtype)
+    dx = tile.shape[2]
+    max_x = new_npx - dx
+    pos_x = np.random.randint(0, max_x, size=batch_size)
+    pos_y = np.random.randint(0, max_x, size=batch_size)
+    for b in range(batch_size):
+        A[b, :, pos_x[b]:pos_x[b]+dx, pos_y[b]:pos_y[b]+dx] = tile[b,...]
+    return A
+
+def mosaic_vis(X, pad=0, save_path=None):
+    xmin, xmax = np.amin(X), np.amax(X)
+    S = (X-xmin)/(xmax-xmin)
+    n = X.shape[0]
+    x = int(np.ceil(np.sqrt(np.float32(n))))
+    y = n // x
+    while x*y<n:
+        y+=1
+    h, w = X.shape[1:3]
+    if len(X.shape)==4:
+        img = np.zeros((h*y+(y-1)*pad, w*x+(x-1)*pad, X.shape[3]))
+        for k,s in enumerate(S):
+            j, i = k//x, k%x
+            img[j*pad+j*h:j*pad+j*h+h, i*pad+i*w:i*pad+i*w+w, :] = s    
+    else:
+        img = np.zeros((h*y+(y-1)*pad, w*x+(x-1)*pad))
+        for k,s in enumerate(S):
+            j, i = k//x, k%x
+            img[j*pad+j*h:j*pad+j*h+h, i*pad+i*w:i*pad+i*w+w] = s
+            
+    if save_path is not None:
+        imsave(save_path, img)
+    return img
